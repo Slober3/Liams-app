706e6f3e128fbb3ed3d27da3f0642567be24c07c
Slober3 <quintendesmyter@hotmail.com>
2023-01-06 23:49:19 +0100

    update to pandas

--- Liams app/liamsapp.py
+++ Liams app/liamsapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request, redirect
 from flask_bootstrap import Bootstrap
-import csv,operator
+import csv
+import pandas as pd
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -19,9 +20,13 @@ def submit():
 @app.route('/')
 def scores():
     rows = []
-    with open('scores.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        data = sorted(reader, key=operator.itemgetter(0), reverse=True)    
-        for row in data:
-            rows.append(row)
-    return render_template('scores.html', rows=rows)
+    dataFrame = pd.read_csv("scores.csv")
+    dataFrame.sort_values(["score"],axis=0, ascending=False,inplace=True,na_position='first')
+
+  #  with open('scores.csv', 'r') as csvfile:
+   #     reader = csv.reader(csvfile)
+    #    data = sorted(reader, key=operator.itemgetter(1), reverse=True)    
+     #   for row in data:
+      #      rows.append(row)
+
+    return render_template('scores.html', tables=[dataFrame.to_html(classes="table table-striped",index=False).replace('border="1"','border="0"').replace('<tr style="text-align: right;">', '<tr>')], titles=[''])
