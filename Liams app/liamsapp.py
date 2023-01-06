e1769e91339a2019758f69663965dfcecc942f73
Slober3 <quintendesmyter@hotmail.com>
2023-01-06 18:06:50 +0100

    first commit

---
+++ Liams app/liamsapp.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request, redirect
+from flask_bootstrap import Bootstrap
+import csv
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/submit', methods=['GET'])
+def submit():
+    score = request.args['score']
+    name = request.args['name']
+
+    # Save the score and name to a database or file, or process the data as needed
+    with open('scores.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([name, score])
+        csvfile.close()
+    return redirect('/')
+
+@app.route('/')
+def scores():
+    rows = []
+    with open('scores.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            rows.append(row)
+    return render_template('scores.html', rows=rows)
